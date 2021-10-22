2998464cc9414124cd0aadb86a4278dcb093eaa7
Gramman87 <gramman87@gmail.com>
2021-10-22 16:43:31 -0600

    pushing jets app

---
+++ src/com/skilldistillery/jets/app/JetsApp.java
@@ -0,0 +1,8 @@
+package com.skilldistillery.jets.app;
+
+import com.skilldistillery.jets.entities.AirField;
+
+public class JetsApp {
+	private AirField airField;
+
+}
--- src/com/skilldistillery/jetsproject/JetsApp.java
+++
@@ -1,5 +0,0 @@
-package com.skilldistillery.jetsproject;
-
-public class JetsApp {
-
-}
