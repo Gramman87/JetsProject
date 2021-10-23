6a083975395415762469c31cfcf5400c7f386fb0
Gramman87 <gramman87@gmail.com>
2021-10-22 19:38:38 -0600

    pushing FighterJet subclass

--- src/com/skilldistillery/jets/entities/FighterJet.java
+++ src/com/skilldistillery/jets/entities/FighterJet.java
@@ -1,5 +1,15 @@
 package com.skilldistillery.jets.entities;
 
-public class FighterJet {
+public class FighterJet extends Jet {
+
+	public FighterJet(String model, double speed, int range, long price) {
+		super(model, speed, range, price);
+		
+	}
+	
+	public void loadCargo() {
+		
+	}
+	
 
 }
--- src/com/skilldistillery/jets/entities/PrivateJet.java
+++ src/com/skilldistillery/jets/entities/PrivateJet.java
@@ -1,11 +1,10 @@
 package com.skilldistillery.jets.entities;
 
 public class PrivateJet extends PassengerJet {
-	private int seatingCap;
 
 	public PrivateJet(String model, double speed, int range, long price, int seatingCap) {
 		super(model, speed, range, price);
-		this.seatingCap = seatingCap;
+		
 	}
 	
 	@Override
